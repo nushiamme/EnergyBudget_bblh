d623c659f338dcd4c521a8e3b6d9991a4fc96eac
Anusha Shankar <nushiamme@gmail.com>
2019-04-06 16:23:06 -0800

    Cleaning up models code and removing unnecessary packages before resubmission

--- Energy_budget_BBLH_models.R
+++ Energy_budget_BBLH_models.R
@@ -4,15 +4,10 @@
 
 ## Reading in packages
 require(ggplot2)
-require(reshape)
-require(plyr)
+#install.packages('plyr') #But DO NOT require() yet because it messes up summarize() in dplyr
 require(dplyr)
-require(ggthemes) ## Trying out Tufteboxplot
 require(stringr)
-require(MCMCglmm)
 require(lme4)
-require(lmerTest)
-require(multcomp) ## for glht function for plotting lme4 results
 
 ## Set working directory
 setwd("C:\\Users\\nushi\\Dropbox\\Anusha Committee\\BBLH_EnergyBudget\\Submission_FuncEcol\\Data/")
@@ -35,12 +30,9 @@ dlw_bblh$DayMonthYear <- paste0(str_pad(dlw_bblh$Day, 2, pad = "0"), "0", dlw_bb
 
 #### New columns/dataframes ####
 colnames(floralsumm)[colnames(floralsumm)=="Pre_post"] <- "Season"
-floralsumm$Season<- revalue(floralsumm$Season, c("Pre"="Dry", "Post"="Early-wet"))
+floralsumm$Season<- plyr::revalue(floralsumm$Season, c("Pre"="Dry", "Post"="Early-wet"))
 flo <- group_by(floralsumm, Site, Transect, Season)
 dflo <- summarize(flo, Flowers = sum(TotalFlowers, na.rm = T))
-#dhumm <- summarize(flo, Hummcount = sum(HummSp, na.rm=T))
-#dfru <- summarize(flo, Fruits = sum(Fruits, na.rm=T))
-#dbud <- summarize(flo, Buds = sum(Buds, na.rm=T))
 dflo$Site_Season <- paste(dflo$Site, dflo$Season, sep="_")
 dflo$Site_Season <- factor(dflo$Season, levels = c("Harshaw_Dry", "Harshaw_Early-wet", 
                                                    "Sonoita_Dry", "Sonoita_Early-wet"))
@@ -50,12 +42,12 @@ flo.season_site_sum <- aggregate(floralsumm$TotalFlowers,
                                  by=list(floralsumm$Season, floralsumm$Site), FUN="sum", na.rm=T)
 names(flo.season_site_sum) <- c("Season", "Site", "Sum_flowers")
 flo.season_site_sum$Site <- droplevels(flo.season_site_sum$Site)
-flo.season_site_sum$Season<- revalue(flo.season_site_sum$Season, c("Pre"="Dry", "Post"="Early-wet"))
-flo.season_site_sum$Site<- revalue(flo.season_site_sum$Site, c("Harshaw"="HC", "Sonoita"="SC"))
+flo.season_site_sum$Site<- plyr::revalue(flo.season_site_sum$Site, c("Harshaw"="HC", "Sonoita"="SC"))
 
 ## Pasting day, month, year together for temp file
 ## Adding zeros before days and months
 m.ta_det$DayMonthYear <- paste0(str_pad(m.ta_det$Day, 2, pad = "0"), "0", m.ta_det$Month, m.ta_det$Year)
+require(plyr)
 temp.summ <- ddply(m.ta_det, .(DayMonthYear, Site), summarise,
       minTemp = min(Ta),
       meanTemp = mean (Ta),
@@ -66,6 +58,7 @@ dlw_merged_models <- merge(dlw_bblh, temp.summ, by=c("Site", "DayMonthYear"))
 
 dlw_merged_models <- merge(dlw_merged_models, flo.season_site_sum, by=c("Site", "Season"))
 
+
 #### Models ####
 DEE_full <- lmer(kJ_day~log(Sum_flowers)+ meanTemp + maxTemp +Initial_mass_g +(1|Site_proxy), REML=F,
                                data= dlw_merged_models)
@@ -75,26 +68,24 @@ plot(DEE_full)
 plot(ranef(DEE_full))
 plot(residuals(DEE_full))
 
-## Taking out season
-#DEE_no_ranef <- lm(kJ_day~log(Sum_flowers)+ meanTemp + maxTemp +Initial_mass_g,
-#                 data= dlw_merged_models)
-#summary(DEE_no_ranef)
-
-
-## Taking out temps
+## Taking out just temps from Full
 DEE_resource_mass <- lmer(kJ_day~log(Sum_flowers) + Initial_mass_g +(1|Site_proxy),  REML=F, data= dlw_merged_models)
 summary(DEE_resource_mass)
 
+## Taking out just mass from Full
 DEE_resource_temps <- lmer(kJ_day~log(Sum_flowers) + meanTemp + maxTemp +(1|Site_proxy),  REML=F, data= dlw_merged_models)
 summary(DEE_resource_temps)
 
+## Removing temps and mass from Full
 DEE_resource <- lmer(kJ_day~log(Sum_flowers) +(1|Site_proxy), REML=F, data= dlw_merged_models)
 summary(DEE_resource)
 
+## Removing resouces and mass from Full
 DEE_temps <- lmer(kJ_day~ meanTemp + maxTemp +(1|Site_proxy), REML=F, data= dlw_merged_models)
 summary(DEE_temps)
 
 ## Best model ### March 19, 2019
+## Simple linear model with just resources, no random effect
 DEE_resource_noranef <- lm(kJ_day~log(Sum_flowers), data= dlw_merged_models)
 summary(DEE_resource_noranef)
 coef(DEE_resource_noranef)
@@ -102,8 +93,3 @@ plot(DEE_resource_noranef)
 
 
 anova(DEE_full, DEE_resource_mass, DEE_resource_temps, DEE_resource, DEE_resource_noranef, DEE_temps)
-
-tmp <- as.data.frame(confint(glht(DEE_resource_mass, mcp(Site_proxy = "Tukey")))$confint)
-tmp$Comparison <- rownames(tmp)
-ggplot(tmp, aes(x = Comparison, y = Estimate, ymin = lwr, ymax = upr)) +
-  geom_errorbar() + geom_point()
